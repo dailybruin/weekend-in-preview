b1a321b6f6c31df01b82ae67872ff0880fdc34ac
Howard Huang <howardhuang96@gmail.com>
2018-02-19 00:55:07 -0800

    preview boxes

--- app.py
+++ app.py
@@ -16,14 +16,16 @@ class Story:
 def home():
     # fake "data" to display
     lorum_ipsum_str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
-    StoryA = Story("http://via.placeholder.com/350x250", "http://dailybruin.com/", "HEADLINE_A", lorum_ipsum_str)
-    StoryB = Story("http://via.placeholder.com/350x250", "http://dailybruin.com/", "HEADLINE_B", lorum_ipsum_str)
-    StoryC = Story("http://via.placeholder.com/350x250", "http://dailybruin.com/", "HEADLINE_C", lorum_ipsum_str)
-    StoryD = Story("http://via.placeholder.com/350x250", "http://dailybruin.com/", "HEADLINE_D", lorum_ipsum_str)
+    StoryA = Story("http://via.placeholder.com/350x250?text=image", "http://dailybruin.com/", "HEADLINE_A", lorum_ipsum_str)
+    StoryB = Story("http://via.placeholder.com/350x250?text=image", "http://dailybruin.com/", "HEADLINE_B", lorum_ipsum_str)
+    StoryC = Story("http://via.placeholder.com/350x250?text=image", "http://dailybruin.com/", "HEADLINE_C", lorum_ipsum_str)
+    StoryD = Story("http://via.placeholder.com/350x250?text=image", "http://dailybruin.com/", "HEADLINE_D", lorum_ipsum_str)
     weekend_roundup_data = [StoryA, StoryB, StoryC, StoryD]
+    preview_data = [StoryA, StoryB, StoryC]
 
     template_data = {
-        "weekend_roundup": weekend_roundup_data
+        "weekend_roundup": weekend_roundup_data,
+        "preview": preview_data
     }
     return render_template('index.html', **template_data)
 
