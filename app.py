85175ada0f714583e34788fd06bfb00dc4f87b2f
Howard Huang <howardhuang96@gmail.com>
2018-02-18 21:24:27 -0800

    create sample data for us to use

--- app.py
+++ app.py
@@ -4,9 +4,26 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# this is what we'll have a story model as
+class Story:
+    def __init__(self, picture, headline, description):
+        self.picture = picture
+        self.headline = headline
+        self.description = description
+
 @app.route("/")
-def template_test():
-    return render_template('index.html')
+def home():
+    # fake "data" to display
+    StoryA = Story("http://via.placeholder.com/350x250", "HEADLINE_A", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore")
+    StoryB = Story("http://via.placeholder.com/350x250", "HEADLINE_B", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore")
+    StoryC = Story("http://via.placeholder.com/350x250", "HEADLINE_C", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore")
+    StoryD = Story("http://via.placeholder.com/350x250", "HEADLINE_D", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore")
+    weekend_roundup_data = [StoryA, StoryB, StoryC, StoryD]
+
+    template_data = {
+        "weekend_roundup": weekend_roundup_data
+    }
+    return render_template('index.html', **template_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
